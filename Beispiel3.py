4dfdfa00e67a4fed6bc00c7b3c9c1dfe84b61f4a
Phillip Lange <phillip@lange-privat.net>
2020-05-20 18:20:34 +0200

    Implementierung Dijkastra und GUI

--- Beispiel3.py
+++ Beispiel3.py
@@ -1,19 +1,20 @@
 import matplotlib.pyplot as plt
 import functools
+from random import randint
 
 #dict to label points
 mylabel = {1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H'}
 
 #dict to derive coordinates
 mylabel2 = {
-         'A':(0,5),
-         'B':(1,0),
-         'C':(5,1),
-         'D':(2,4),
-         'E':(3,6),
-         'F':(0,7),
-         'G':(4,8),
-         'H':(6,2)
+         'A':(randint(1,10),randint(1,10)),
+         'B':(randint(1,10),randint(1,10)),
+         'C':(randint(1,10),randint(1,10)),
+         'D':(randint(1,10),randint(1,10)),
+         'E':(randint(1,10),randint(1,10)),
+         'F':(randint(1,10),randint(1,10)),
+         'G':(randint(1,10),randint(1,10)),
+         'H':(randint(1,10),randint(1,10))
            }
 #Adjancency and State Matrix
 Adj_Matrix = [[0, 20, 0, 0, 0, 0, 15, 0],
@@ -28,13 +29,17 @@ Adj_Matrix = [[0, 20, 0, 0, 0, 0, 15, 0],
 xCoord=[mylabel2[k][0] for k in sorted(mylabel2)]
 yCoord=[mylabel2[k][1] for k in sorted(mylabel2)]
 plt.plot(xCoord, yCoord, 'bo')
-plt.axis([-1, 7, -1, 9])
+plt.axis([-1, 11, -1, 11])
+
 for i in range(8):
     plt.text(xCoord[i]-0.5, yCoord[i], mylabel[i+1])
+
 for i in range(8):
     for j in range(8):
         if Adj_Matrix[i][j]:
             plt.plot([xCoord[i], xCoord[j]],[yCoord[i], yCoord[j]], 'b')
+
+"""
 #Dijkstra Algorithm
 def dijkstra(graph,start,target):
     inf = functools.reduce(lambda x,y: x+y,(i[1] for u in graph for i
@@ -62,14 +67,14 @@ in graph[u]))
     return " -> ".join(trav),dist[target]
 
 graph = {
-    'A' : [('B',20), ('G', 15)],
-    'B' : [('A', 20),('C', 8), ('D', 9)],
-    'C' : [('B', 8),('D', 6), ('E', 15), ('H', 10)],
-    'D' : [('B', 9),('C', 6),('E', 7)],
-    'E' : [('C', 15),('D', 7),('F', 22),('G', 18)],
-    'F' : [('E', 22)],
-    'G' : [('A', 15),('E', 18)],
-    'H' : [('C', 10)]
+    'A' : [('B', randint(1,20)), ('G', randint(1,20))],
+    'B' : [('A', randint(1,20)),('C', randint(1,20)), ('D', randint(1,20))],
+    'C' : [('B', randint(1,20)),('D', randint(1,20)), ('E', randint(1,20)), ('H', randint(1,20))],
+    'D' : [('B', randint(1,20)),('C', randint(1,20)),('E', randint(1,20))],
+    'E' : [('C', randint(1,20)),('D', randint(1,20)),('F', randint(1,20)),('G', randint(1,20))],
+    'F' : [('E', randint(1,20))],
+    'G' : [('A', randint(1,20)),('E', randint(1,20))],
+    'H' : [('C', randint(1,20))]
     }
 traverse, dist = dijkstra(graph,'F','H')
 print( traverse)
@@ -77,5 +82,5 @@ print( traverse)
 mydrawing = traverse.split('-> ')
 plt.plot([ mylabel2[n.rstrip()][0] for n in mydrawing ],[
 mylabel2[n.rstrip()][1] for n in mydrawing])
-print ("Distance:",dist)
+print ("Distance:",dist)"""
 plt.show()
--- Main.py
+++ Main.py
@@ -1,1 +1,99 @@
-print("Hello World!")
+#---------Imports-------- 
+import matplotlib.pyplot as plt
+import functools
+from random import randint, choice
+from math import sqrt
+
+def dijkstra(verbindungsliste,start,ende):
+    distanz = dict()
+    herkunftsliste = dict()
+    warteschlange = list()
+    for i in verbindungsliste.keys():                                                                               #initialisiert Distanzliste mit "unendlich"
+        distanz[i] = float('inf')                                                                    
+        herkunftsliste[i] = 0                                                                                       #initialisiert Dictionary mit Distanzen zu vorherigen Sachen mit 0
+        warteschlange.append(i)                                                                                     #Liste mit allen Knotennamen
+    
+    distanz[start] = 0                                                                                              #Distanz zum Startpunkt ist 0
+    while len(warteschlange) != 0:                                                                                  #so lange Elemente vorhanden sind:
+        minimum = min(warteschlange, key=lambda x: distanz[x])                                                      #wähle Knoten aus wo die Distanz am kleinsten ist
+        warteschlange.remove(minimum)                                                                               #entfernt Element, damit es nicht nochmal überprüft werden kann
+        for verbindung, strecke in verbindungsliste[minimum]:
+            alternative = distanz[minimum] + strecke                                                                #Distanz aus kürzestem Weg + neue kürzeste Route zum nächsten Knoten
+            if alternative < distanz[verbindung]:                                                                   #Wenn neue Route kleiner der alten ist
+                distanz[verbindung] = alternative                                                                   #setze Distanz zu neuem Knoten als Distanz
+                herkunftsliste[verbindung] = minimum                                                                #merke dir den Punkt, den du vorher angesteuert hast
+    weg = [ende]
+    temporaer = ende
+    while temporaer != start:
+        weg.append(herkunftsliste[temporaer])
+        temporaer = herkunftsliste[temporaer]
+    weg.reverse()
+    return weg, distanz[ende]
+
+def Pythagoras(Punkt1, Punkt2):
+    distanz = round(sqrt(((Punkt1[0]-Punkt2[0])**2 + (Punkt1[1] - Punkt2[1])**2)))
+    if distanz == 0:
+        return 1
+    else:
+        return distanz
+
+def ErstelleKoordiantenliste(knotenzahl = 8):
+    koordinatenliste = dict()
+    for i in range(knotenzahl):
+        koordinatenliste[i] = [randint(1, 5*knotenzahl), randint(1, 5*knotenzahl)]
+    print(koordinatenliste)
+    return koordinatenliste
+
+def ErstelleVerbindungsliste(koordinatenliste, knotenzahl = 8):
+    verbindungsliste = dict()
+    for i in koordinatenliste.keys():
+        verbindungsliste[i] = list()
+    for i in koordinatenliste.keys():
+        if knotenzahl < 20:
+            k = randint(1,2)
+        else: 
+            k= randint(1, knotenzahl // 10 + 1)
+        genutzteverbindungsliste = []
+        for j in range(k):
+            verbindung = choice([n for n in range(knotenzahl) if (n != i) and (n not in genutzteverbindungsliste)])
+            genutzteverbindungsliste.append(verbindung)
+            distanz = Pythagoras(koordinatenliste[i], koordinatenliste[verbindung])
+            verbindungsliste[i] = verbindungsliste[i] + [(verbindung, distanz)]
+            verbindungsliste[verbindung] = verbindungsliste[verbindung] + [(i, distanz)]
+    print(verbindungsliste)
+    return verbindungsliste
+
+def connectpoints(x,y,p1,p2):
+    x1, x2 = x[p1], x[p2]
+    y1, y2 = y[p1], y[p2]
+    plt.plot([x1,x2],[y1,y2],':ko')
+
+def ShowPlot(koordinatenliste, verbindungsliste, weg, knotenzahl = 8):
+    xCoord=[koordinatenliste[k][0] for k in sorted(koordinatenliste)]
+    yCoord=[koordinatenliste[k][1] for k in sorted(koordinatenliste)]
+
+    for i in verbindungsliste.keys():
+        for n in range(len(verbindungsliste[i])):
+            connectpoints(xCoord, yCoord, i, verbindungsliste[i][n][0])
+
+    plt.axis([-1, knotenzahl * 5 + 1, -1, knotenzahl * 5 + 1])
+
+    for i in range(knotenzahl):
+        plt.text(xCoord[i]-0.5, yCoord[i], str(i))
+
+    plt.plot([koordinatenliste[n][0] for n in weg ],[koordinatenliste[n][1] for n in weg], '-r')
+
+    plt.show()
+    
+
+
+koordinatenliste = ErstelleKoordiantenliste()
+verbindungsliste = ErstelleVerbindungsliste(koordinatenliste)
+knotenzahl = 8
+startkoordinate = randint(0,knotenzahl)
+endkoordinate = choice([n for n in range(knotenzahl) if (n !=  startkoordinate)])
+
+weg, distanz = dijkstra(verbindungsliste,startkoordinate, endkoordinate)
+print(weg)
+print ("Distance:",distanz)
+ShowPlot(koordinatenliste, verbindungsliste, weg, knotenzahl=8) 
